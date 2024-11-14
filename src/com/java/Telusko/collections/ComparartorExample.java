f3b5b2bc63c8a395ccea0d0ad4ec4459eba4875f
vinay nagisetty <83533201+vinaynagisetty@users.noreply.github.com>
2024-11-14 11:53:09 +0000

    done custom sorting based on length

--- src/com/java/Telusko/collections/ComparartorExample.java
+++ src/com/java/Telusko/collections/ComparartorExample.java
@@ -8,23 +8,51 @@ import java.util.List;
 
 public class ComparartorExample {
     public static void main(String[] args) {
-        List<Integer> studentMarks=new ArrayList<>();
-        studentMarks.add(59);
-        studentMarks.add(22);
-        studentMarks.add(10);
+        // List<Integer> studentMarks=new ArrayList<>();
+        // studentMarks.add(59);
+        // studentMarks.add(22);
+        // studentMarks.add(10);
 
-        Comparator<Integer> compFunction =new Comparator<Integer>(){
-            public int compare(Integer i,Integer j){
-                if(i%10 > j%10){
-                    return 1;
-                }
+        // Comparator<Integer> compFunction =new Comparator<Integer>(){
+        //     public int compare(Integer i,Integer j){
+        //         if(i%10 > j%10){
+        //             return 1;
+        //         }
+        //        return -1;
+        //     }
+        // };
+        // // Collections.sort(studentMarks); for automatic sort
+        // //for custome sort
+        // Collections.sort(studentMarks,compFunction);
+        // System.out.println(studentMarks);
+        Comparator<String> lengthComp=new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                // TODO Auto-generated method stub
+               if(o1.length() <o2.length()){
+                return 1;
+               }
                return -1;
             }
+            
         };
-        // Collections.sort(studentMarks); for automatic sort
-        //for custome sort
-        Collections.sort(studentMarks,compFunction);
-        System.out.println(studentMarks);
+
+        List<String> name=new ArrayList<>();
+        name.add("vinay");
+        name.add("akashssss");
+        name.add("vina");
+   
+        //basic sorting 
+
+        // Collections.sort(name);
+        // System.out.println(name);
+
+        //custom sorting based on the length
+
+        Collections.sort(name,lengthComp);
+         System.out.println(name);
+
     }
 
 }
